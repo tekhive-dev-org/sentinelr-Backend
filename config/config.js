b42b3551ca02f762aaee41fd29b7264764f49d29
Babasola Oso <easybabasola@gmail.com>
2026-01-27 15:40:13 +0100

    conn stg 3

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
     logging: false
   },
   production: {
-    use_env_variable: DB_URL,
+    use_env_variable: 'DB_URL',
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
