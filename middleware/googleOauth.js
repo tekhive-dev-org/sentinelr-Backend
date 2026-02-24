179642fd03d4b6a6b6c0b0acaded4b3b0da21bb1
Babasola Oso <easybabasola@gmail.com>
2026-02-24 19:45:41 +0100

    mounted the OAuth file

--- middleware/googleOauth.js
+++ middleware/googleOauth.js
@@ -7,7 +7,7 @@ const User = require('../models/User')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    callbackURL: "api/auth/google/callback"
+    callbackURL: "/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
