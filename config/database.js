59247c32e3f1221b7aeb080f47b4da835768e1ac
Babasola Oso <babasola.oso@cbitechnologiesltd.com>
2026-04-22 09:29:52 +0100

    reduced idle

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
             max: 2,    // switched from 10 to 5, then from 5 to 2, so render doesnt hit Supabase's limits quickly
             min: 0,
             acquire: 60000,     // switch from 30000 to 60000, to wait longer
-            idle: 10000
+            idle: 3000        // switch from 10000 to 3000, to keep idle connections alive longer
         }
     })
 } 
