d50bcb2ebcf071529388eea48449dfbcebb053e5
Babasola Oso <babasola.oso@cbitechnologiesltd.com>
2026-04-22 14:05:39 +0100

    removed pool event

--- config/database.js
+++ config/database.js
@@ -21,28 +21,6 @@ if (process.env.NODE_ENV === "production") {
             idle: 3000        // switch from 10000 to 3000, to keep idle connections alive longer
         }
     })
-
-    const pool = dbConnection.connectionManager.pool
-
-    pool.on('acquire', (connection) => {
-        console.log(`Connection ${connection.processID || ''} 📫A-C-Q-U-I-R-E-D  A-T :📫 ${new Date().toISOString()}`)
-    })
-
-    pool.on('release', (connection) => {
-        console.log(`Connection ${connection.processID || ''} 🚿R-E-L-E-A-S-E-D  A-T :🚿 ${new Date().toISOString()}`)
-    })
-
-    pool.on('destroy', (connection) => {
-        console.log(`Connection ${connection.processID || ''} 💥D-E-S-T-R-O-Y-E-D  A-T :💥 ${new Date().toISOString()}`);
-    })
-
-    pool.on('connect', (connection) => {
-        console.log(`Connection ${connection.processID || ''} 🔌C-O-N-N-E-C-T-E-D !🔌`)
-    })
-
-    pool.on('disconnect', (connection) => {
-        console.log(`Connection ${connection.processID || ''} ⛓️‍💥D-I-S-C-O-N-N-E-C-T-E-D !⛓️‍💥`)
-    })
 }
 else {
     dbConnection = new Sequelize(
