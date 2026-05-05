05941e90a9bc328e49d4837d350f8c4a5b845d2e
Babasola Oso <babasola.oso@cbitechnologiesltd.com>
2026-05-05 10:35:01 +0100

    setinterval

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV === "production") {
         dialect: "postgres",
         logging: (msg) => console.log(`[Sequelize] ${msg}`),
         dialectOptions: {
+            keepAlive: true,
             statement_timeout: 0,
             ssl: {
                 require: true,
@@ -15,20 +16,22 @@ if (process.env.NODE_ENV === "production") {
             }
         },
         pool: {
-            max: 5,    // switched from 10 to 5, then from 5 to 2, so render doesnt hit Supabase's limits quickly
+            max: 10,    // switched from 10 to 5, then from 5 to 2, so render doesnt hit Supabase's limits quickly
             min: 0,
             acquire: 30000,     // switch from 30000 to 60000, to wait longer
-            idle: 10000        // switch from 10000 to 3000, to keep idle connections alive longer
+            idle: 10000,        // switch from 10000 to 3000, to keep idle connections alive longer
+            evict: 10000
         }
     })
 }
 else {
-    dbConnection = new Sequelize(process.env.DB_URL,
+    dbConnection = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
         {
             host: process.env.DB_HOST,
             logging: console.log,
-            ssl: false,
-            rejectUnauthorized: false,
             // logging: false,
             // dialect: 'mysql'
             // url: process.env.DATABASE_URL
@@ -37,5 +40,15 @@ else {
         }
     )
 }
+
+setInterval(async () => {
+  try {
+    await dbConnection.query('SELECT 1')
+  } catch (err) {
+    console.error('❌ DB ping failed:', err.message)
+  }
+}, 120000)
+
+
 
 module.exports = dbConnection
